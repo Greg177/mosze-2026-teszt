ef3d02c6a3b0663156c614a1ab0adef6598870f3
Greg177 <szepgergo13@gmail.com>
2026-02-28 10:15:17 +0100

    Hibák megjelölve kommentekkel

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // HIBA: NELEMENTS helyett N_ELEMENTS kell
+	std::cout << '1-100 ertekek duplazasa' // HIBA: sima ' helyett " kell, illetve nincs lezárva a vége ;-jellel
+    for (int i = 0;) // HIBA: hiányzó feltétel és léptető
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // HIBA: feltétel mindig false, soha nem fut le
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // HIBA: hiányzó << b[i] és ;
     }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    std::cout << "Atlag szamitasa: " << std::endl; 
+    int atlag; // HIBA: nincs inicializálva, legyen = 0
+    for (int i = 0; i < N_ELEMENTS, i++) // HIBA: vessző helyett ;
     {
-        atlag += b[i]
+        atlag += b[i] // HIBA: hiányzó ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
