e48bd67372c47381a29ff947da1b6f1c0743a9ff
Greg177 <szepgergo13@gmail.com>
2026-02-28 10:19:22 +0100

    Hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // HIBA: NELEMENTS helyett N_ELEMENTS kell
-	std::cout << '1-100 ertekek duplazasa' // HIBA: sima ' helyett " kell, illetve nincs lezárva a vége ;-jellel
-    for (int i = 0;) // HIBA: hiányzó feltétel és léptető
+    int *b = new int[N_ELEMENTS]; // HIBA: NELEMENTS helyett N_ELEMENTS kell
+    std::cout << "1-100 ertekek duplazasa"; // HIBA: sima ' helyett " kell, illetve nincs lezárva a vége ;-jellel
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: hiányzó feltétel és léptető
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // HIBA: feltétel mindig false, soha nem fut le
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: feltétel mindig false, soha nem fut le
     {
-        std::cout << "Ertek:" // HIBA: hiányzó << b[i] és ;
+        std::cout << "Ertek: " << b[i] << std::endl; // HIBA: hiányzó << b[i] és ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl; 
-    int atlag; // HIBA: nincs inicializálva, legyen = 0
-    for (int i = 0; i < N_ELEMENTS, i++) // HIBA: vessző helyett ;
+    int atlag = 0; // HIBA: nincs inicializálva, legyen = 0
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: vessző helyett ;
     {
-        atlag += b[i] // HIBA: hiányzó ;
+        atlag += b[i]; // HIBA: hiányzó ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
